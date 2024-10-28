3f45f0dc45d9dd2eabc12fa53a5d07a5fbdcf180
Sai Ashray Reddy <146927680+SaiAshrayReddy@users.noreply.github.com>
2024-10-27 22:06:21 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import React from "react";
+import { useState } from "react";
+import PassCheck from "./components/Password";
+
+function App() {
+  return (
+    <>
+      <PassCheck />
+    </>
+  );
+}
+
+export default App;
